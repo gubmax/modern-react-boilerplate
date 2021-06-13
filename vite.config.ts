852d4a41109cda6b30c15acf47e3b2c49c599c2c
gubmax <gubyshev@gmail.com>
2021-06-13 17:11:56 +0300

    Add eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import eslint from '@rollup/plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [
+    reactRefresh(),
+    { ...eslint(), enforce: 'pre', apply: 'build' },
+  ],
   resolve: {
     alias: {
       src: path.resolve(__dirname, '/src'),
