46494c12e3f50731456da598d4874a4de8cbfa36
gubmax <gubyshev@gmail.com>
2021-06-14 17:50:16 +0300

    Build with Preact

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   arrowParens: 'always',
-  printWidth: 120,
+  printWidth: 100,
   singleQuote: true,
   semi: false,
   tabs: false,
--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,33 @@ import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 import eslint from '@rollup/plugin-eslint'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    { ...eslint({ throwOnError: true }), enforce: 'pre', apply: 'build' },
-    reactRefresh(),
-    vanillaExtractPlugin(),
-  ],
-  resolve: {
-    alias: {
-      src: path.resolve(__dirname, '/src'),
-      server: path.resolve(__dirname, '/server'),
+export default ({ mode }) => {
+  const preactMode = mode === 'preact'
+
+  return defineConfig({
+    plugins: [
+      { ...eslint({ throwOnError: true }), enforce: 'pre', apply: 'build' },
+      reactRefresh(),
+      vanillaExtractPlugin(),
+    ],
+    resolve: {
+      alias: {
+        src: path.resolve(__dirname, '/src'),
+        server: path.resolve(__dirname, '/server'),
+        ...(preactMode ? {
+          react: 'preact/compat',
+          'react-dom': 'preact/compat',
+        } : {}),
+      },
     },
-  }
-})
+    esbuild: {
+      ...(preactMode ? {
+          jsxFactory: 'h',
+          jsxFragment: 'Fragment',
+          jsxInject: 'import { h, Fragment } from "preact"',
+        } : {
+          jsxInject: 'import React from "react"',
+        }),
+    },
+  })
+}
