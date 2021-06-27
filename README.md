966ce321dea7e281e9c0e3e325d639ede5e74194
gubmax <gubyshev@gmail.com>
2021-06-27 11:40:55 +0300

    Remove preact support

--- README.md
+++ README.md
@@ -15,8 +15,5 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 ### `npm run build`
 Build for production.
 
-### `npm run build:preact`
-Build for production with **Preact instead of React**.
-
 ### `npm run serve`
 Locally preview production build.
--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,6 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
  * @link https://vitejs.dev/config/
  */
 const config: UserConfigFn = ({ mode }) => {
-  const preactMode = mode === 'preact'
-
   return {
     plugins: [
       reactRefresh(),
@@ -16,21 +14,11 @@ const config: UserConfigFn = ({ mode }) => {
       alias: {
         src: path.resolve(__dirname, '/src'),
         server: path.resolve(__dirname, '/server'),
-        ...(preactMode ? {
-          react: 'preact/compat',
-          'react-dom': 'preact/compat',
-        } : {}),
       },
     },
     esbuild: {
-      ...(preactMode ? {
-          jsxFactory: 'h',
-          jsxFragment: 'Fragment',
-          jsxInject: 'import { h, Fragment } from "preact"',
-        } : {
-          jsxFactory: 'jsx',
-          jsxInject: 'import { jsx } from "@emotion/react"',
-        }),
+      jsxFactory: 'jsx',
+      jsxInject: 'import { jsx } from "@emotion/react"',
     },
     build: {
       minify: false
