45a23e313c9c9dd5db8d723eff12c34012990016
gubmax <gubyshev@gmail.com>
2021-08-14 10:22:14 +0300

    Remove extra type

--- server/config/env.config.ts
+++ server/config/env.config.ts
@@ -2,5 +2,5 @@ export const CONFIG_ENV = {
   isTestEnv: process.env.NODE_ENV === 'test' || !!process.env.VITE_TEST_BUILD,
   isProdEnv: process.env.NODE_ENV === 'production',
   logLevel: process.env.LOG_LEVEL || 'trace',
-  port: parseInt(process.env.PORT, 10) || 3000,
+  port: (process.env.PORT && parseInt(process.env.PORT, 10)) || 3000,
 }
