6b368e6e9f196cc96f3616588c5ad77af728b2cc
gubmax <gubyshev@gmail.com>
2021-09-18 14:03:31 +0300

    build: update packages

--- src/components/layout/Main/Main.constants.tsx
+++ src/components/layout/Main/Main.constants.tsx
@@ -1,9 +1,9 @@
-import { PartialRouteObject } from 'react-router'
+import { RouteObject } from 'react-router'
 
 import { AboutPage, CartPage, SettingsPage } from 'src/components/pages'
 import { PageRoutes } from 'src/infra/http'
 
-export const ROUTES: PartialRouteObject[] = [
+export const ROUTES: RouteObject[] = [
   { path: PageRoutes.ROOT, element: <AboutPage /> },
   { path: PageRoutes.CART, element: <CartPage /> },
   { path: PageRoutes.SETTINGS, element: <SettingsPage /> },
