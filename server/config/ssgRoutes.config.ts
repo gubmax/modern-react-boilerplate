38608c794672e3f3a00cc7cbd3f1caa36b75572f
gubmax <gubyshev@gmail.com>
2021-09-21 00:42:14 +0300

    fix: ssr instead of not found page

--- server/config/ssgRoutes.config.ts
+++ server/config/ssgRoutes.config.ts
@@ -7,5 +7,5 @@ export const CONFIG_SSG_ROUTES: Record<string, string> = {
   // route: filename
   [PageRoutes.ROOT]: 'about',
   [PageRoutes.SETTINGS]: 'settings',
-  '*': 'notFound',
+  $$NOT_FOUND: 'notFound',
 }
