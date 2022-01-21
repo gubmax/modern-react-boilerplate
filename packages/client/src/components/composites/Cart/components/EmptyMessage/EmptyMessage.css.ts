b7312b22ff43fcdf787ff3f50c9f16a2040a6820
gubmax <gubyshev@gmail.com>
2022-01-21 23:59:31 +0300

    fix(client): cart empty message layout

--- packages/client/src/components/composites/Cart/components/EmptyMessage/EmptyMessage.css.ts
+++ packages/client/src/components/composites/Cart/components/EmptyMessage/EmptyMessage.css.ts
@@ -3,6 +3,9 @@ import { style } from '@vanilla-extract/css'
 import { vars } from 'client/src/common/styles'
 
 export const wrapper = style({
+  flexDirection: 'column',
+  display: 'flex',
+  alignItems: 'center',
   textAlign: 'center',
   padding: vars.space.s4,
 })
