f9d3f06680370550b149ad58187f90e54f19262b
gubmax <gubyshev@gmail.com>
2022-02-23 11:12:06 +0300

    build: add bundle visualizer

--- packages/client/config/vite.config.ts
+++ packages/client/config/vite.config.ts
@@ -3,10 +3,13 @@ import { UserConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 import { PATH_DIST_CLIENT } from '../../shared/constants/paths'
 import { HtmlEntries } from '../../server/src/common/constants/html'
 import manifest from '../public/manifest.json'
+
+const VISUALIZE_MODE = !!process.env.VISUALIZE_MODE
 
 const ENTRY_PATH_MAIN = resolve(__dirname, `../src/${HtmlEntries.MAIN}.tsx`)
 const ENTRY_PATH_INTERNAL_ERROR = resolve(__dirname, `../src/${HtmlEntries.INTERNAL_ERROR}.tsx`)
@@ -16,7 +19,18 @@ const ENTRY_PATH_INTERNAL_ERROR = resolve(__dirname, `../src/${HtmlEntries.INTER
  */
 const config: UserConfig = {
   publicDir: resolve(__dirname, '../public'),
-  plugins: [react(), vanillaExtractPlugin(), VitePWA({ manifest, registerType: 'autoUpdate' })],
+  plugins: [
+    react(),
+    vanillaExtractPlugin(),
+    !VISUALIZE_MODE && VitePWA({ manifest, registerType: 'autoUpdate' }),
+    VISUALIZE_MODE &&
+      visualizer({
+        filename: '../../visualizer/stats.html',
+        open: true,
+        gzipSize: true,
+        projectRoot: resolve(__dirname, '../../'),
+      }),
+  ].filter(Boolean),
   resolve: {
     alias: {
       client: resolve(__dirname, '../../client'),
@@ -24,8 +38,8 @@ const config: UserConfig = {
     },
   },
   build: {
-    manifest: true,
-    emptyOutDir: true,
+    manifest: !VISUALIZE_MODE,
+    emptyOutDir: !VISUALIZE_MODE,
     outDir: resolve(__dirname, `../../../${PATH_DIST_CLIENT}`),
     rollupOptions: {
       input: {
